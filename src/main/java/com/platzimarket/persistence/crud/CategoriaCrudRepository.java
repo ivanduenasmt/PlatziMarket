d4624397660ff612e895bce303b50d835a79b7e5
Iván Dueñas <ivanduenias@gmail.com>
2022-06-01 14:47:32 -0500

    Sexto commit: Uso de las interfaces Crud repository

---
+++ src/main/java/com/platzimarket/persistence/crud/CategoriaCrudRepository.java
@@ -0,0 +1,8 @@
+package com.platzimarket.persistence.crud;
+
+import com.platzimarket.persistence.entity.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaCrudRepository extends CrudRepository<Categoria,Integer>{
+
+}
