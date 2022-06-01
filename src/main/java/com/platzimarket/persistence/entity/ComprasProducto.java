5cf260dab30ce6fc2bdf8fa87ead755a5aedeb37
Iván Dueñas <ivanduenias@gmail.com>
2022-06-01 14:07:59 -0500

    Cuarto commit: Creación de entity con clave primaria compuesta

---
+++ src/main/java/com/platzimarket/persistence/entity/ComprasProducto.java
@@ -0,0 +1,20 @@
+package com.platzimarket.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+
+    private Double total;
+
+    private Boolean estado;
+
+}
