472324f6063e0c44fa1e111b9e088785619728e8
Iván Dueñas <ivanduenias@gmail.com>
2022-06-01 15:03:04 -0500

    Septimo commit: Query methods para producto

--- src/main/java/com/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzimarket/persistence/ProductoRepository.java
@@ -2,9 +2,12 @@ package com.platzimarket.persistence;
 
 import com.platzimarket.persistence.crud.ProductoCrudRepository;
 import com.platzimarket.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -13,5 +16,16 @@ public class ProductoRepository {
         return (List<Producto>) productoCrudRepository.findAll();
     }
 
-    
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> GetEscasos(int stock, boolean estado) {
+        return productoCrudRepository.findByStockLessThanAndEstado(stock,true);
+    }
+
+    public List<Producto> findByPrecioVenta(int precioVenta){
+        return productoCrudRepository.findByPrecioVentaOrderByAsc(precioVenta);
+    }
+
 }
--- src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzimarket/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,11 @@ package com.platzimarket.persistence.crud;
 import com.platzimarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByStockLessThanAndEstado(int stock, boolean estado);
+    List<Producto> findByPrecioVentaOrderByAsc(int precioVenta);
 }
